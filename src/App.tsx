944b01182db4864284bce95a0a6b361631fd5ff9
Vladimir Bykov <milsinery@icloud.com>
2021-04-18 21:01:54 +0300

    add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import avatar from '../src/avatar.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Main__wrapper">
+      <div className="App__avatar">
+        <img src={avatar} alt="avatar" />
+      </div>
+      <div className="App__links">
+        <a href="https://t.me/pluginsforfigma" target="_blank" rel="noopener noreferrer">Канал, где я рассказываю про свои плагины — Plugins for Figma</a>
+        <a href="https://t.me/milsinery" target="_blank" rel="noopener noreferrer">Инста</a>
+        <a href="https://behance.net/milsinery" target="_blank" rel="noopener noreferrer">Биханс</a>
+        <a href="https://instagram.com/milsinery" target="_blank" rel="noopener noreferrer">Телега</a>
+        <a href="https://career.habr.com/milsinery" target="_blank" rel="noopener noreferrer">Профиль на Хабр Карьере</a>
+        <a href="mailto:milsinery@icloud.com">milsinery@icloud.com</a>
+      </div>
+      </div>
     </div>
   );
 }
