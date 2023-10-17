42d4b2c73b42d926a0387de95057358f612de5f4
Vladimir Bykov <milsinery@icloud.com>
2023-10-17 13:45:09 +0200

    updated prop logo added karina page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import Main from './pages/main/index';
+import Karina from './pages/karina/index';
 import Plugins from './pages/plugins/index';
 import Plugin from './pages/plugin/index';
 import Records from './pages/records/index';
@@ -9,11 +10,11 @@ const App = () => {
   return (
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/karina" component={Karina} />
         <Route exact path="/plugins" component={Plugins} />
         <Route exact path="/plugins/:name" component={Plugin} />
         <Route exact path="/blog" component={Records} />
         <Route exact path="/blog/:name" component={Record} />
-        <Route component={Main} />
       </Switch>
   );
 };
---
+++ src/pages/karina/index.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+const Karina = () => {
+  return (
+    <div className='gift'>
+      <div className="background">
+        <div className="card">
+          <div className="first"></div>
+          <div className="second"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Karina;
