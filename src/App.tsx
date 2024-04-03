aea580c75ef5bb088620d98c9645029d70b865db
Vladimir Bykov <milsinery@icloud.com>
2024-04-03 15:56:00 +0200

    add danila page as a joke

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { Route, Switch } from 'react-router-dom';
 import Main from './pages/main/index';
-import Karina from './pages/karina/index';
 import Plugins from './pages/plugins/index';
 import Plugin from './pages/plugin/index';
 import Records from './pages/records/index';
 import Record from './pages/record/index';
+import Karina from './pages/karina/index';
+import Danila from './pages/danila/index';
 
 const App = () => {
   return (
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/karina" component={Karina} />
         <Route exact path="/plugins" component={Plugins} />
         <Route exact path="/plugins/:name" component={Plugin} />
         <Route exact path="/blog" component={Records} />
         <Route exact path="/blog/:name" component={Record} />
+        <Route exact path="/karina" component={Karina} />
+        <Route exact path="/danila" component={Danila} />
       </Switch>
   );
 };
---
+++ src/pages/danila/index.tsx
@@ -0,0 +1,20 @@
+const Danila = () => {
+  return (
+    <main className="main effect-fade-in effect-zoom-in">
+
+    <div className="main__wrapper">
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+      <h1 className="main__title">Пойдём покурим, заебал!</h1>
+    </div>
+  </main>
+  );
+};
+
+export default Danila;
