464103bfbd7ab11a9de880faccef15c5875d95ab
Vladimir Bykov <milsinery@icloud.com>
2024-06-26 12:55:21 +0200

    changed default icons and manifest

--- src/utils/setMetaTags.js
+++ src/utils/setMetaTags.js
@@ -1,14 +1,21 @@
 export const setMetaTags = (iconPath, manifestPath) => {
     // Set icon
     const link = document.querySelector("link[rel~='icon']");
+    const appleIcon = document.querySelector("link[rel~='apple-touch-icon']");
 
     if (link) {
         link.href = iconPath;
+        appleIcon.href = iconPath;
     } else {
         const newLink = document.createElement('link');
         newLink.rel = 'icon';
         newLink.href = iconPath;
         document.head.appendChild(newLink);
+
+        const newAppleLink = document.createElement('link');
+        newLink.rel = 'apple-touch-icon';
+        newLink.href = iconPath;
+        document.head.appendChild(newAppleLink);
     }
 
     // Set manifest
